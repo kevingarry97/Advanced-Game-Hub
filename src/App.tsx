8f1be64963f5658042525e56feaae2be94d01d95
Kevin Garry <kevingarry97@gmail.com>
2023-07-13 14:42:09 +0200

    Implementing Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,9 @@ function App() {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem bg="blue.300" area={"aside"}>
-          Aside
-        </GridItem>
+        <GridItem area={"aside"}>Aside</GridItem>
       </Show>
-      <GridItem bg="green.300" area={"main"}>
-        Main
-      </GridItem>
+      <GridItem area={"main"}>Main</GridItem>
     </Grid>
   );
 }
